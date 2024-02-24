d12ad13c250092d3c279b9043eebb8ec5031d0e4
afu-codemonday <sikarin.p@codemonday.com>
2024-02-25 02:00:45 +0700

    using cdn

--- src/index.web.tsx
+++ src/index.web.tsx
@@ -3,12 +3,13 @@ import "@expo/metro-runtime"
 import { LoadSkiaWeb } from "@shopify/react-native-skia/lib/module/web"
 import { App } from "expo-router/build/qualified-entry"
 import { renderRootComponent } from "expo-router/build/renderRootComponent"
+import { version } from 'canvaskit-wasm/package.json'
 
 // This file should only import and register the root. No components or exports
 // should be added here.
 LoadSkiaWeb({
   locateFile: (file) => {
-    return `/${file}`
+    return `https://cdn.jsdelivr.net/npm/canvaskit-wasm@${version}/bin/full/${file}`
   },
 }).then(async () => {
   renderRootComponent(App)
